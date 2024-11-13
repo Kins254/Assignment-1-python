9e68162dd59f3ab8e42b09fcd94240373fb3c99f
Erick <erickgitau250@gmail.com>
2024-11-13 10:44:38 +0300

    correction2

--- index.py
+++ index.py
@@ -5,17 +5,17 @@ num2=float(input("Enter the second number:"))
 
 if operand == "+":
     result = num1 + num2
-    print("The answer is:", result)
+    print(num1,operand,num2, "=",result)
 elif operand == "-":
     result = num1 - num2
-    print("The answer is:", result)
+    print(num1,operand,num2, "=",result)
 elif operand=="*":
     result =num1 * num2
     print("The answer is:",result)
 elif operand=="/":
     if num2 != 0:
         result = num1 / num2
-        print("The answer is:", result)
+        print(num1,operand,num2, "=",result)
     else:
         print("Error: Cannot divide by zero.")
 else :
